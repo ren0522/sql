8e678a9eb18e057fa630504be4ae4485968cf540
ren0522 <ren_@kakao.com>
2022-06-13 16:20:47 +0900

    2022-06-13 수업예제

---
+++ 1_SQL 기본실습.sql
@@ -0,0 +1,42 @@
+#날짜 : 2022/06/13
+#이름 : 김재원
+#내용 : SQL 기본실습
+
+#실습하기 1-1
+CREATE DATABASE `MyDB`;
+DROP DATABASE `MyDB`;
+
+#실습하기 1-2
+CREATE TABLE `user1` (
+`uid` VARCHAR(10),
+`name` varchar(10),
+`hp` CHAR(13),
+`age` TINYINT
+);
+
+DROP TABLE `user1`;
+
+#실습하기 1-3
+INSERT INTO `user1` VALUES ('A101', '김유신', '010-1234-1111', 25);
+INSERT INTO `user1` VALUES ('A102', '김춘추', '010-1234-2222', 23);
+INSERT INTO `user1` VALUES ('A103', '장보고', '010-1234-3333', 32);
+INSERT INTO `user1` (`uid`,`name`,`age`) VALUES ('A104', '강감찬', 45);
+INSERT INTO `user1` SET `uid`='A105', `name`='이순신', `hp`='010-1234-5555';
+
+
+#실습하기 1-4
+SELECT * FROM `user1`;
+SELECT * FROM `user1` WHERE `uid`='A101';
+SELECT * FROM `user1` WHERE `name`='김춘추';
+SELECT * FROM `user1` WHERE `age` > 30;
+SELECT `uid`, `name`, `age` FROM `user1`;
+
+#실습하기 1-5
+UPDATE `user1` SET `hp`='010-1234-4444' WHERE `uid`='A104';
+UPDATE `user1` SET `age`=51 WHERE `uid`='A105';
+UPDATE `user1` SET `hp`='010-1234-1001',`age`=27 WHERE `uid`='A101';
+
+#실습하기 1-6
+DELETE FROM `user1` WHERE `uid`='A101';
+DELETE FROM `user1` WHERE `uid`='A102' AND `age`=25;
+DELETE FROM `user1` WHERE `age`>=30;
